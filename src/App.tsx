c539e0d48dbcdfb74f04b1d3b9ad721bc9bb7f2b
dg0397 <rgg0397@gmail.com>
2020-11-09 22:42:43 -0400

    add shapes

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,91 @@
-import React, { useState } from 'react';
-import { Board, Winner  } from './Board'
-import styled from 'styled-components'
-import { StartScreen } from './StartScreen';
-import { ResetScreen } from './ResetScreen'
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { Board, Winner } from "./Board";
+import styled from "styled-components";
+import { StartScreen } from "./StartScreen";
+import { ResetScreen } from "./ResetScreen";
+import { motion } from "framer-motion";
 
 const variants = {
-  start: { 
-    opacity: 1, 
-    scale: 1, 
-    width: "200px", 
-    height: "100px", 
-    transition: { 
-      type: "spring", 
-      duration : '0.8'
-    }
+  start: {
+    opacity: 1,
+    scale: 1,
+    width: "200px",
+    height: "100px",
+    transition: {
+      type: "spring",
+      duration: "0.8",
+    },
   },
-  hidden : { 
-    opacity : 0, 
-    scale : 0.8
+  hidden: {
+    opacity: 0,
+    scale: 0.8,
   },
-  game : {
-    width : "500px",
-    height : "500px",
-    transition: { 
-      type: "spring", 
-      duration : '0.8'
-    }
+  game: {
+    width: "500px",
+    height: "500px",
+    transition: {
+      type: "spring",
+      duration: "0.8",
+    },
   },
-  reset: {  
-    width: "300px", 
-    height: "200px", 
-    transition: { 
-      type: "spring", 
-      duration : '0.8'
-    }
-  }
-}
+  reset: {
+    width: "300px",
+    height: "200px",
+    transition: {
+      type: "spring",
+      duration: "0.8",
+    },
+  },
+};
 
-const BoardContainer = styled(motion.div).attrs(()=>({
-  initial:'hidden',
-  variants
+const BoardContainer = styled(motion.div).attrs(() => ({
+  initial: "hidden",
+  variants,
 }))`
-  display:flex;
+  display: flex;
   justify-content: center;
   align-items: center;
   background-color: white;
-  border-radius:16px;
-  box-shadow: -6px 10px 30px 4px rgba(0,0,0,.3);
-  border:15px solid white;
+  border-radius: 16px;
+  box-shadow: -6px 10px 30px 4px rgba(0, 0, 0, 0.3);
+  border: 15px solid white;
+`;
+
+const Heading = styled.h1`
+  color: white;
+  text-align:center;
+  margin-bottom: 1rem;
+  text-shadow: -3px 3px #00000066
 `
-type GameState = 'start' | 'game' | 'reset';
+type GameState = "start" | "game" | "reset";
 
 function App() {
-  const [ winner,setWinner ] = useState<Winner>();
-  const [ gameState,setGameState ] = useState<GameState>('start');
+  const [winner, setWinner] = useState<Winner>();
+  const [gameState, setGameState] = useState<GameState>("start");
 
-  const onGameEnd = (winner:Winner) => {
-    setWinner(winner)
-    setGameState('reset')
-  }
+  const onGameEnd = (winner: Winner) => {
+    setWinner(winner);
+    setGameState("reset");
+  };
 
   const onReset = () => {
-    setWinner(undefined)
-    setGameState('game')
-  }
+    setWinner(undefined);
+    setGameState("game");
+  };
   return (
-    <BoardContainer animate = {gameState}>
-      {
+    <>
+      <Heading>TypeScript Tic-Tac-Toe</Heading >
+      <BoardContainer animate={gameState}>
         {
-          start : <StartScreen onStart = {() => setGameState('game')}/>,
-          game : <Board onGameEnd = { onGameEnd }/>,
-          reset : <ResetScreen winner = {winner} onReset = { onReset } />,
-        }[gameState]
-      }
-    </BoardContainer>
-  )
+          {
+            start: <StartScreen onStart={() => setGameState("game")} />,
+            game: <Board onGameEnd={onGameEnd} />,
+            reset: <ResetScreen winner={winner} onReset={onReset} />,
+          }[gameState]
+        }
+      </BoardContainer>
+    </>
+  );
 }
 
 export default App;
--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,5 +1,8 @@
 import React, { FC, ReactElement } from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import circle from "./circle.svg";
+import cross from "./cross.svg";
 
 const CellWrapper = styled.button`
   background-color: white;
@@ -7,6 +10,28 @@ const CellWrapper = styled.button`
   box-shadow: none;
   cursor: pointer;
   outline: none;
+  position: relative;
+`;
+
+const variants = {
+  hidden: {
+    opacity: 0.5,
+    transform: "translate3d(-50%, -50%, 0) scale(0.5)",
+  },
+  visible: {
+    opacity: 1,
+    transform: "translate3d(-50%, -50%, 0) scale(1)",
+  },
+};
+const Shape = styled(motion.img).attrs(() => ({
+  initial: "hidden",
+  variants,
+}))`
+  height: 60px;
+  width: 60px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
 `;
 export type CellValue = "X" | "O" | undefined;
 
@@ -19,7 +44,11 @@ type CellProps = {
 export const Cell: FC<CellProps> = ({ value, toggle, index }): ReactElement => {
   return (
     <CellWrapper onClick={() => toggle(index)}>
-      {value === "O" ? "O" : value ? "X" : null}
+      {value === "O" ? (
+        <Shape animate="visible" src={circle} />
+      ) : value ? (
+        <Shape animate="visible" src={cross} />
+      ) : null}
     </CellWrapper>
   );
 };
